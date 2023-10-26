e87e2d6fac8ebc62d29521ea87b265d6574c12ad
jonathan castillo <gamepolis.1234@gmail.com>
2023-10-26 04:35:57 -0600

    agregando comentarios al codigo

--- chatbot.py
+++ chatbot.py
@@ -17,3 +17,10 @@ print(bot.saludo())
 
 # El bot se despide
 print(bot.despedida())
+
+# Este código crea una clase ChatBot con métodos para saludar y despedirse.
+# Luego crea una instancia de ChatBot llamada BotPython y utiliza sus métodos
+# para saludar y despedirse.
+# Por favor, ten en cuenta que este es un ejemplo muy básico. Un bot de chat real
+#  requeriría mucho más código y funcionalidad, como la capacidad de procesar
+#  y responder a las entradas del usuario.
